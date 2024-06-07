f5d1c939477195fc8735b533f8a4fcc9ad169034
Emerson <151971499+emersonramos3lt@users.noreply.github.com>
2024-06-07 17:21:59 -0300

    Delete ex07-click-events directory

--- ex07-click-events/main.jsx
+++
@@ -1,9 +0,0 @@
-// React é uma biblioteca de JS, e não um framework
-
-// --------- Como iniciar um React? ----------
-
-// Primeiro você abre um novo terminal, e você irá escrever (cd my-react-app) o nome após o cd é você que irá escolher quando você faz a instalação, após isso aperte a tecla enter.
-
-// Após você escrever esse nome você irá escrever na linha de baixo, você irá escrever (npm run dev) e aperte a tecla enter, pará o progama ocorrer.
-
-// Após isso haverá um arquivo que foi adicionado, o arquivo está com o nome (my-react-app), ele tem node_modules, public onde fica o svg do vite, que é uma logo e asset onde contem imagens e videos
