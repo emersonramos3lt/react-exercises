d0d79d1191434f9867383b126ad1c727337a1260
Emerson <151971499+emersonramos3lt@users.noreply.github.com>
2024-06-07 17:23:24 -0300

    New

---
+++ ex03-add-css/App.jsx
@@ -0,0 +1,22 @@
+// HOW TO STYLE REACY COMPONENTS WITH CSS
+
+// (not including external frameworks or preprocessors)
+
+// 1. EXTERNAL
+// 2. MODULES
+// 3. INLINE
+
+import Button from './Button.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Button/>
+    </>
+  
+);
+   
+}
+
+export default App
